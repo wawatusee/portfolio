1dca15c06abdf5b0510b6ffe15788dd18d5b13e1
wawatusee <web2020.kieran.labarrere@gmail.com>
2020-06-25 17:55:03 +0200

    Exemples de galerie à tester page galerie

--- p-galerie/galerie.php
+++ p-galerie/galerie.php
@@ -29,7 +29,17 @@
             <!-- CONTENU PRINCIPAL -->
             <main>
                 <p>La page de galerie présenterait des screenshot de mes différentes anciennes productions, accompagnées d'un commentaire. Forme simple pour pouvoir tester différentes css<br>
-                Une div galerie, Une liste de lien, ds chaque lien, une vignette et un texte avec un display particulier pour le texte.</p>
+                Une div galerie, Une liste de lien, ds chaque lien, une vignette et un texte avec un display particulier pour le texte. Le modèle de wikipedia a la forme simple que je cherche pour la structure <br>
+                Je vais tenter de l'intégrer, lien vers la page : <a href="https://fr.wikipedia.org/wiki/Aide:Galerie">Galerie wikipedia</a></p>
+                <gallery>
+                Fichier:p-galeries/img/Tomato.svg|Une légende.
+                Fichier:Tomato.svg|Une légende.
+                Fichier:Tomato.svg|Une légende.
+                Fichier:Tomato.svg|Une légende avec un [[Wikipédia:Liens internes|lien]].
+                Fichier:Tomato.svg|Une légende.
+                Fichier:Tomato.svg|Une légende.
+                </gallery>
+                <p>Ou le modèle flex de try it yourself mais avec une interface en JSON comme ci-dessus, plutot que  écrit direct depuis le php : <a href="https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_image_grid">Galerie Flex</a></p>
             </main>
             <!-- BARRE LATERALE -->
             <aside>
