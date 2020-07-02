beab6d5aa3e4ea1ba9ac89300c6ee7182c145b4f
wawatusee <web2020.kieran.labarrere@gmail.com>
2020-07-03 01:19:14 +0200

    Tuto, première étape html de Canvas

--- pages/tuto.php
+++ pages/tuto.php
@@ -20,9 +20,8 @@
                 <Hn>Portfolio</Hn>
                 <ul>
                 <!--ICI une liste de liens des rubriques de la page-->
-                    <li>A créer une liste des sous rubriques de la page principale, par exemple pour la page tuto, la liste des tutos.<br>
-                Ne nous inquiétons pas pour l'instant de la largeur de cette section, au final son contenu ne devrait pas excéder la largeur d'un mot cliquable.<br>
-                Pour la version mobile peut-être un menu repliable à gauche, à voir.
+                    <li>
+                        <a href="#tutoriel1">Tutoriel1-CANVAS</a>
                     </li>
                 </ul>
             </nav>
@@ -30,27 +29,44 @@
             <main>
                 <p>2 tutos sont demandés, dois-je placer chaque tuto dans un répertoire particulier? Je bloque pas pour l'instant, j'attend de voir les contraintes de chaque tuto selon son contenu.</p>
                 <section>
-                <h2>Tutoriel1-CANVAS</h2>
-                <ol>
-                    <li>Qu'est ce que CANVAS?</li>
-                    <li>A quoi ça sert?</li>
-                    <li>Comment le mettre en place</li>
-                    <li>Comment le mettre en place?</li>
-                    <li>Comment l'utiliser?
-                        <ul>Création
-                            <li>Page html</li>
-                            <li>Div dédiée à CANVAS(id)</li>
-                            <li>Script JS</li>
-                        </ul>
-                    </li>
-                </ol>
-                <h3>Qu'est ce que CANVAS?</h3>
-                <p>Dans le cadre de pages html, l'élément &lsaquo;canvas&rsaquo;est utilisé pour dessiner des formes à la volée grace à java-script.</p>
-                <h3>A quoi ça sert?</h3>
-                <p>La balise html &lsaquo;canvas&rsaquo; n'est qu'un conteneur pour vos formes ou dessins, c'est le java-script qui va vous permettre de créer les formes contenues.</p>
-                <h3>Comment le mettre en place?</h3>
-                <p>Le premier élément à mettre en place est votre balise &lsaquo;canvas&rsaquo;</p>
-                <code>'<canvas id="myCanvas" width="200" height="100"></canvas>'</code>
+                    <h2 id="tutoriel1">Tutoriel1-CANVAS</h2>
+                    <ol>
+                        <li>Qu'est ce que CANVAS?</li>
+                        <li>A quoi ça sert?</li>
+                        <li>Comment le mettre en place, l'utiliser?
+                            <ul>
+                                <li>Div html</li>
+                                <li>id et Script JS</li>
+                            </ul>
+                        </li>
+                    </ol>
+                    <h3>Qu'est ce que CANVAS?</h3>
+                    <p>Dans le cadre de pages html, l'élément &lsaquo;canvas&rsaquo;est utilisé pour dessiner des formes à la volée grace à java-script.</p>
+                    <h3>A quoi ça sert?</h3>
+                    <p>La balise html &lsaquo;canvas&rsaquo; est un conteneur pour des formes ou dessins, le java-script permet d'y importer des formes pleines,ou juste leurs contours ou de les dessiner point à point.</p>
+                    <h3>Comment le mettre en place?</h3>
+                    <p>Le premier élément à mettre en place est votre balise &lsaquo;canvas&rsaquo; <br>
+                    Une balise ouvrante, une balise fermante canvas <br>
+                    à laquelle nous donnons un attribut largeur(width="200"), une propriété hauteur(height="200")<br>
+                    </p>
+                    <code>&lsaquo;canvas width="200" height="200"&rsaquo;&lsaquo;/canvas&rsaquo;</code>
+                    <div id="comment">Le carré ci-dessous est votre balise canvas : <br>
+                    <br>
+                    </div>
+                    <div class="renducode">
+                        <canvas width="200" height="200"></canvas>
+                    </div>
+                    <p>Le premier élément à mettre en place est votre balise &lsaquo;canvas&rsaquo;</p>
+                    <code>&lsaquo;canvas width="200" height="200"&rsaquo;&lsaquo;/canvas&rsaquo;</code>
+                    <div id="comment">Le carré ci-dessous est votre balise canvas : <br>
+                    Une balise ouvrante, une balise fermante canvas <br>
+                    avec une propriété largeur(width="200"), <br>
+                    une propriété hauteur(height="200")<br>
+                    et un id (id="CanvasEnHtml") qui va nous permettre de cibler le contenu de ce carré grace au Java-script. <br>
+                    </div>
+                    <div class="renducode">
+                        <canvas width="200" height="200"></canvas>
+                    </div>
                 </section>
             </main>
             <!-- BARRE LATERALE -->
