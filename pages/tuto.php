5bcff7471d84d1d884f0db09519f6dde8fa2d819
wawatusee <web2020.kieran.labarrere@gmail.com>
2020-07-02 13:18:26 +0200

    Début du tuto canvas dans la page tuto

--- pages/tuto.php
+++ pages/tuto.php
@@ -29,6 +29,29 @@
             <!-- CONTENU PRINCIPAL -->
             <main>
                 <p>2 tutos sont demandés, dois-je placer chaque tuto dans un répertoire particulier? Je bloque pas pour l'instant, j'attend de voir les contraintes de chaque tuto selon son contenu.</p>
+                <section>
+                <h2>Tutoriel1-CANVAS</h2>
+                <ol>
+                    <li>Qu'est ce que CANVAS?</li>
+                    <li>A quoi ça sert?</li>
+                    <li>Comment le mettre en place</li>
+                    <li>Comment le mettre en place?</li>
+                    <li>Comment l'utiliser?
+                        <ul>Création
+                            <li>Page html</li>
+                            <li>Div dédiée à CANVAS(id)</li>
+                            <li>Script JS</li>
+                        </ul>
+                    </li>
+                </ol>
+                <h3>Qu'est ce que CANVAS?</h3>
+                <p>Dans le cadre de pages html, l'élément &lsaquo;canvas&rsaquo;est utilisé pour dessiner des formes à la volée grace à java-script.</p>
+                <h3>A quoi ça sert?</h3>
+                <p>La balise html &lsaquo;canvas&rsaquo; n'est qu'un conteneur pour vos formes ou dessins, c'est le java-script qui va vous permettre de créer les formes contenues.</p>
+                <h3>Comment le mettre en place?</h3>
+                <p>Le premier élément à mettre en place est votre balise &lsaquo;canvas&rsaquo;</p>
+                <code>'<canvas id="myCanvas" width="200" height="100"></canvas>'</code>
+                </section>
             </main>
             <!-- BARRE LATERALE -->
             <aside>
